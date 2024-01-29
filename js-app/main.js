417a86e0460b7bdd650a00a8985c127f26e8e1dc
Colin Matteson <seefm@proton.me>
2024-01-29 13:31:47 -0500

    applies updates to solution for chapter 2 challenge book 3 web api/c#/sql backend curriculum newforce

--- js-app/main.js
+++ js-app/main.js
@@ -1,26 +1,38 @@
 const url = "https://localhost:44346/api/beanvariety/";
+const coffeeurl = "https://localhost:44346/api/coffee/";
 
 const button = document.querySelector("#run-button");
+const coffeeButton = document.querySelector("#coffee-button");
 const results = document.querySelector("#results");
 const beanForm = document.querySelector("#beanForm");
 button.addEventListener("click", () => {
     getAllBeanVarieties()
         .then(beanVarieties => {
             console.log(beanVarieties);
+            results.innerHTML = "";
             for (let i = 0; i < beanVarieties.length; i++) {
                 results.innerHTML += `<br /> ${beanVarieties[i].name} ${beanVarieties[i].region} <br /> `
             }
         })
 });
 
-beanForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-
-})
+coffeeButton.addEventListener("click", () => {
+    getAllCoffees()
+        .then(beanVarieties => {
+            console.log(beanVarieties);
+            results.innerHTML = "";
+            for (let i = 0; i < beanVarieties.length; i++) {
+                results.innerHTML += `<br /> ${beanVarieties[i].title} <br /> `
+            }
+        })
+});
 
 function getAllBeanVarieties() {
     return fetch(url).then(resp => resp.json());
+}
+
+function getAllCoffees() {
+    return fetch(coffeeurl).then(resp => resp.json());
 }
 
 function addNewBean(event) {
