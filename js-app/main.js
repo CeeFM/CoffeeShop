05353de45a241d9e99e6e42240dd5a13be19526e
Colin Matteson <seefm@proton.me>
2024-01-28 20:37:56 -0500

    completes exercises 1 and 3 in chapter two book 3 web api in newforce backend curriculum

--- js-app/main.js
+++ js-app/main.js
@@ -1,10 +1,14 @@
 const url = "https://localhost:44346/api/beanvariety/";
 
 const button = document.querySelector("#run-button");
+const results = document.querySelector("#results");
 button.addEventListener("click", () => {
     getAllBeanVarieties()
         .then(beanVarieties => {
             console.log(beanVarieties);
+            for (let i = 0; i < beanVarieties.length; i++) {
+                results.innerHTML += `<br /> ${beanVarieties[i].name} ${beanVarieties[i].region} <br /> `
+            }
         })
 });
 
